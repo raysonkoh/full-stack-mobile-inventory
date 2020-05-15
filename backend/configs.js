29dcbe08fe83f37f5c0893dc8e4fb1ffb38b52cd
Rayson Koh <koh_rayson@yahoo.com.sg>
2020-05-15 16:43:50 +0800

    Delete configs.js

--- backend/configs.js
+++
@@ -1,7 +0,0 @@
-const secret = 'secret';
-const mongoURI = 'mongodb+srv://db-admin:P@ssw0rd123@test-db-sytcx.mongodb.net/test?retryWrites=true&w=majority';
-
-module.exports = {
-  secret, 
-  mongoURI
-};
